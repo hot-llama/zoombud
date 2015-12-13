e93a3f5962681ab2a52fd5cc652bc05468592033
Josh Milo <josh@joshmilo.com>
2015-12-12 22:50:03 -0800

    Removes (g) from data store, strips extra characters from price/qty
    This gets our price and quantity data in a format we can use for the
    order. Still have to connect the function for the submit

--- client/components/menuform/menuform.jsx
+++ client/components/menuform/menuform.jsx
@@ -1,53 +1,54 @@
 MenuForm = React.createClass({
 
-    getInitialState() {
+  getInitialState() {
     var price = _.first(this.props.dataPrice[0]);
     var qty = this.props.dataPrice[0][1];
 
-      return {
-          qty: "1",
-          size: `${price} / ${qty}`
-      };
-    },
+    return {
+      qty: "1",
+      size: [price,qty]
+    };
+  },
 
-    getDefaultProps() {
-        return {
-            qtySize: ""
-        }
-    },
-
-    onChangeSize(e) {
-      var self = this;
-      this.setState({size: e.target.value}, function(){
-        self.props.onValueChange(self.state.size);
-      });
-    },
-
-    onChangeQty(e) {
-        var self = this;
-        this.setState({qty: e.target.value}, function(){
-            self.props.onValueChange(self.state.qty);
-        });
-    },
-
-    orderSubmitHandler(e) {
-        e.preventDefault();
-        console.log(this.state.qty, this.state.size);
-    },
-
-    render() {
-        return (
-            <div className="menuForm">
-            <label className="item item-input">
-                <input type="text" placeholder="Qty" ref="qtyInput" onChange={this.onChangeQty}  />
-            </label>
-            <select className="menu-select" onChange={this.onChangeSize} value={this.state.value}>
-                {this.props.dataPrice.map(function (result, id) {
-                   return <option key={id}>{result[0]} / {result[1]}</option>
-                })}
-            </select>
-                <button className="plus-button pull-right ion-plus-circled button button-outline button-balanced" onClick={this.orderSubmitHandler}></button>
-            </div>
-        )
+  getDefaultProps() {
+    return {
+      qtySize: ""
     }
+  },
+
+  onChangeQty(e) {
+    var self = this;
+    this.setState({qty: e.target.value}, function () {
+      self.props.onValueChange(self.state.qty);
+    });
+  },
+
+  onChangeSize(e) {
+    var self = this;
+    this.setState({size: _.words(e.target.value)}, function () {
+      self.props.onValueChange(self.state.size);
+    });
+  },
+
+  orderSubmitHandler(e) {
+    e.preventDefault();
+    console.log(this.state.qty, "-", this.state.size);
+  },
+
+  render() {
+    return (
+      <div className="menuForm">
+        <label className="item item-input">
+          <input type="text" placeholder="Qty" ref="qtyInput" onChange={this.onChangeQty}/>
+        </label>
+        <select className="menu-select" onChange={this.onChangeSize} value={this.state.value}>
+          {this.props.dataPrice.map(function (result, id) {
+            return <option key={id}>{result[0]} / {result[1]} (g)</option>
+          })}
+        </select>
+        <button className="plus-button pull-right ion-plus-circled button button-outline button-balanced"
+                onClick={this.orderSubmitHandler}></button>
+      </div>
+    )
+  }
 })
