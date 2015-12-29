5c991a68088383890202621718e7a11c9fd4f26f
Josh Milo <josh@joshmilo.com>
2015-12-28 18:41:01 -0800

    Adds some basic styling to order form

--- client/components/orderForm/orderform.jsx
+++ client/components/orderForm/orderform.jsx
@@ -1,9 +1,21 @@
 OrderForm = React.createClass({
   render() {
     return (
-      <div className="container">
-        <p>Fud U</p>
-        <a href="/">Back</a>
+      <div className="padding ionic-body">
+        <MenuHeader />
+
+        <div className="content-wrapper">
+          <a className="button icon-left ion-chevron-left button-clear button-dark" href="/">Back</a>
+          <h1>Cart</h1>
+          <div className="row">
+            <div className="col">
+              <ul className="list">
+                <li className="item">Item</li>
+                <li className="item">Item</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
