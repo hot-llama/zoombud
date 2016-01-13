4822967dd24a43eb01be879a4619099562a86986
Josh Milo <josh@joshmilo.com>
2016-01-12 21:34:38 -0800

    adds order button and basic function

--- client/components/menubutton.jsx
+++ client/components/menubutton.jsx
@@ -1,14 +1,4 @@
 MenuButton = React.createClass({
-  /**
-   * Submits order to twilio service.
-   *
-   */
-  submitOrder() {
-    //let order = Orders.findOne({}, {sort: {dateOrdered: -1}});
-    //consuela.log(order);
-    console.log(this.props.order);
-  },
-
   render() {
     return (
       <a href={this.props.route} className={this.props.btnClass} type={this.props.btnType}>{this.props.btnText}
